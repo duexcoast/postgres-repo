81b397a3260bbf949a48f45302dea88324228193
duexcoast <conor.ux@gmail.com>
2022-10-14 16:36:55 -0400

    created user repo. convert snake into camelCase

---
+++ src/repos/user-repo.js
@@ -0,0 +1,36 @@
+const pool = require('../pool');
+
+class UserRepo {
+  static async find() {
+    const { rows } = await pool.query('SELECT * FROM users;');
+
+    const parsedRows = rows.map((row) => {
+      const replaced = {};
+
+      for (let key in row) {
+        const camelCase = key.replace(/[-_][a-z]/gi, ($1) =>
+          $1.toUpperCase().replace('_', '')
+        );
+        replaced[camelCase] = row[key];
+      }
+      return replaced;
+    });
+
+    return parsedRows;
+  }
+
+  static async findById(id) {
+    // const {rows} = await pool.query(`
+    //   SELECT * FROM users
+    //   WHERE id =
+    // `)
+  }
+
+  static async insert() {}
+
+  static async update() {}
+
+  static async delete() {}
+}
+
+module.exports = UserRepo;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const UserRepo = require('../repos/user-repo');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {});
+router.get('/users', async (req, res) => {
+  //Run a query to get all users
+  const users = await UserRepo.find();
+
+  // send the result back
+  res.send(users);
+});
 
 router.post('/users', async (req, res) => {});
 
