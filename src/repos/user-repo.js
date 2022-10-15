c08f83b53116332c84c315ad948ce9076e498802
conor.ney <conor.ux@gmail.com>
2022-10-14 22:22:42 -0400

    test for create user

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -5,23 +5,61 @@ class UserRepo {
   static async find() {
     const { rows } = await pool.query('SELECT * FROM users;');
     return toCamelCase(rows);
-    return rows;
   }
 
   static async findById(id) {
-    // REALLY BIG SECURITY ISSUE
-    const { rows } = await pool.query(`
-      SELECT * FROM users
-      WHERE id = ${id} 
-    `);
+    const { rows } = await pool.query(
+      `
+      SELECT * FROM users WHERE id = $1;
+    `,
+      [id]
+    );
     return toCamelCase(rows)[0];
   }
 
-  static async insert() {}
+  static async insert(username, bio) {
+    const { rows } = await pool.query(
+      `
+      INSERT INTO users (username, bio)
+      VALUES
+        ($1, $2) RETURNING *;
+    `,
+      [bio, username]
+    );
+    return toCamelCase(rows);
+  }
 
-  static async update() {}
+  static async update(id, username, bio) {
+    const { rows } = await pool.query(
+      `
+     UPDATE users 
+     SET username = $1, bio = $2
+     WHERE id = $3
+     RETURNING *;
+    `,
+      [username, bio, id]
+    );
+    return toCamelCase(rows)[0];
+  }
 
-  static async delete() {}
+  static async delete(id) {
+    const { rows } = await pool.query(
+      `
+      DELETE FROM users
+      WHERE id = $1
+      RETURNING *;
+    `,
+      [id]
+    );
+    return toCamelCase(rows)[0];
+  }
+
+  static async count() {
+    const { rows } = await pool.query(`
+      SELECT COUNT(*) FROM users;
+    `);
+    return rows[0].count;
+  }
 }
 
 module.exports = UserRepo;
